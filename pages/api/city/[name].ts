e9653868b106b21749949f36e70f31574b7d7b2c
koad7 <kodjo.adanledji@gmail.com>
2023-06-13 16:59:30 +0200

    weather icon placed

--- pages/api/city/[name].ts
+++ pages/api/city/[name].ts
@@ -4,14 +4,15 @@ import cities from '@/lib/city.list.json';
 
 const Cities = cities as CityData[];
 const NUM_SUGGESTIONS = 5;
+
 function searchCities(value: string): CityData[] {
     const matchingCities = Cities.filter(city =>
         city.name.toLowerCase().startsWith(value.toLowerCase())
     ).slice(0, NUM_SUGGESTIONS);
     return matchingCities;
 }
-export default function handler({ query: { name } }: NextApiRequest, res:
-    NextApiResponse) {
+
+export default function handler({ query: { name } }: NextApiRequest, res:NextApiResponse) {
     const cityName = Array.isArray(name) ? name.join('') : name;
     // Filter the list of cities to those whose name contains the given name
     const filteredCities = cityName ? searchCities(cityName) : [];
