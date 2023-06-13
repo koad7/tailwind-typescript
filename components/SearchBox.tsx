9c55714d07c443d09d3003709915e398ff97e185
koad7 <kodjo.adanledji@gmail.com>
2023-06-13 16:28:57 +0200

    init

---
+++ components/SearchBox.tsx
@@ -0,0 +1,10 @@
+export default function SearchBox() {
+    return (
+        <>
+            <input
+                className="bg-gray-200 p-2 rounded-lg w-64"
+                type="text"
+                placeholder="City name" />
+        </>
+    )
+}
